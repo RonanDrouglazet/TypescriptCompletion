d1eaf18dfa36b3d84bd598712b694637b14bc176
Ronan Drouglazet <ronan.drouglazet@ebuzzing.com>
2014-01-07 23:04:40 +0100

    Add Constant, debug log, and relative project path support (more test needed)

--- bin/TSCompletion.py
+++ bin/TSCompletion.py
@@ -3,16 +3,44 @@ import logging
 import sublime, sublime_plugin
 
 class TscompletionCommand(sublime_plugin.TextCommand):
+
+    ## Constant plugin
+    defaultFileEncoding = "utf-8"
+    extInclude = ".ts" #TODO do a list
+    extExclude = ".d.ts" #TODO do a list
+    osDelimiter = "/" #TODO dif between os ? actually OSX
+
+    ## Variable plugin
     projectPathList = []
     tsFileList = []
     tsClassList = []
 
+    ## Method plugin
+    def run(self, edit):
+        self.projectPathList = self.getCurrentProjectPath()
+        self.tsFileList = self.getTsFileList(self.projectPathList)
+        self.tsClassList = self.genClassList(self.tsFileList)
+        sublime.active_window().show_quick_panel(self.tsFileList, self.on_choice)
+
     def getCurrentProjectPath(self):
-        projectFolderList = sublime.active_window().project_data()["folders"] #user project currently open folders
+        projectFolderList = sublime.active_window().project_data()["folders"]
         dirList = []
 
         for pathDic in projectFolderList:
-            dirList.append(pathDic["path"])
+
+            # Absolute path => ok
+            if os.path.isdir(pathDic["path"]):
+                dirList.append(pathDic["path"])
+
+            # Relative path => not ok
+            else:
+                userPathList = sublime.packages_path().rsplit(self.osDelimiter)
+                userPath = self.osDelimiter + self.osDelimiter.join((userPathList[1], userPathList[2], "Documents")) + self.osDelimiter
+                #userPath = self.osDelimiter + userPathList[1] + self.osDelimiter +userPathList[2] + self.osDelimiter + "Documents" + self.osDelimiter
+                if os.path.isdir(userPath + pathDic["path"]):
+                    dirList.append(userPath + pathDic["path"])
+
+        #logging.warning("Project Path: " + str(dirList))
 
         return dirList
 
@@ -21,9 +49,10 @@ class TscompletionCommand(sublime_plugin.TextCommand):
         for path in pathList:
             for root, dirs, files in os.walk(path):
                 for name in files:
-                    if name.endswith(".ts") & (name.endswith(".d.ts") == False):
-                        fileList.append(os.path.join(root, name))
-                        #logging.warning(os.path.join(root, name))
+                    if name.endswith(self.extInclude) & (not name.endswith(self.extExclude)):
+                            fileList.append(os.path.join(root, name))
+
+        #logging.warning("File List: " + str(fileList))
 
         return fileList
 
@@ -32,19 +61,11 @@ class TscompletionCommand(sublime_plugin.TextCommand):
         for file in fileList:
             classList.append(self.extractClassAndMethodFromFile(file))
 
+        #logging.warning("Class List: " + str(classList))
+
     def extractClassAndMethodFromFile(self, file):
-        logging.warning(file)
-        tmpFile = open(file, 'r', -1, 'utf-8')
-        test = tmpFile.readlines()
+        tmpFile = open(file, 'r', -1, self.defaultFileEncoding)
         tmpFile.close()
 
-
-    def run(self, edit):
-        self.projectPathList = self.getCurrentProjectPath()
-        self.tsFileList = self.getTsFileList(self.projectPathList)
-        self.tsClassList = self.genClassList(self.tsFileList)
-
-        sublime.active_window().show_quick_panel(self.tsFileList, self.on_choice)
-
     def on_choice(self, value):
-        logging.warning("Finish:" + self.tsFileList[value])
+        logging.warning("Choosen value:" + self.tsFileList[value])
